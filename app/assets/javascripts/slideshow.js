c6a5179e561fc073508df8bb3b0d88044e9c89e8
Ricardo Espinel Ardila <ricardoespinel@gmail.com>
2015-05-15 18:53:53 -0500

    Heroku config

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -10,3 +10,14 @@ $('.banner').unslider({
 	dots: true,               //  Display dot navigation
 	fluid: false              //  Support responsive design. May break non-responsive designs
 });
+
+<script>
+    var unslider = $('.banner').unslider();
+    
+    $('.unslider-arrow').click(function() {
+        var fn = this.className.split(' ')[1];
+        
+        //  Either do unslider.data('unslider').next() or .prev() depending on the className
+        unslider.data('unslider')[fn]();
+    });
+</script>
