be7cf6c98667396f450a40958eca447acd40b128
Ricardo Espinel Ardila <ricardoespinel@gmail.com>
2015-05-20 10:40:57 -0500

    Heroku config

--- vendor/assets/javascripts/slideshow.js
+++ vendor/assets/javascripts/slideshow.js
@@ -2,14 +2,3 @@ $(function() {
     $('.banner').unslider();
 });
 
-
-<script>
-    var unslider = $('.banner').unslider();
-    
-    $('.unslider-arrow').click(function() {
-        var fn = this.className.split(' ')[1];
-        
-        //  Either do unslider.data('unslider').next() or .prev() depending on the className
-        unslider.data('unslider')[fn]();
-    });
-</script>
